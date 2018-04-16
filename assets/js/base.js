d49ac18f2b650f29045f0b0575e8cc71e972fc86
aifly <aifly_xc@163.com>
2018-04-16 18:49:48 +0800

    a

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,9 +1,12 @@
 var imgs = {
 	grass :"./assets/images/grass.png",
 	demo:'./assets/images/demo.png',
+	humidity:'./assets/images/humidity.png',
+	temperature:'./assets/images/temperature.png',
 	cInfo:'./assets/images/c-info.png',
 	cTag:'./assets/images/c-tag.png',
-	infoBg:'./assets/images/info-bg.png'
+	infoBg:'./assets/images/info-bg.png',
+	line:'./assets/images/line.png'
 
 }
 window.config = {
--- components/baseinfo/index.vue
+++ components/baseinfo/index.vue
@@ -1,62 +1,107 @@
 <template>
 	<div  class="symbin-baseinfo-main-ui lt-full">
 		<Header></Header>
-
-		<div class="symbin-main-title">
+		<section class="symbin-baseinfo-wrap" :style='{height: viewH-108 + "px"}' ref='page'>
 			<div>
-				<img :src="imgs.demo" />
-			</div>
-			<div>
-				<div class="symbin-id">
-					{{baseInfo.Id}}
-				</div>
-				<img :src="imgs.cInfo" />
-				<div class="symbin-baseinfo-date">
-					<div class="symbin-birthday">
-						生于：{{baseInfo.birthday}}
+				<div class="symbin-main-title">
+					<div>
+						<img :src="imgs.demo" />
 					</div>
-					<div class="symbin-dieday">
-						卒于：{{baseInfo.dieday}}
+					<div>
+						<div class="symbin-id">
+							{{baseInfo.Id}}
+						</div>
+						<img :src="imgs.cInfo" />
+						<div class="symbin-baseinfo-date">
+							<div class="symbin-birthday">
+								生于：{{baseInfo.birthday}}
+							</div>
+							<div class="symbin-dieday">
+								卒于：{{baseInfo.dieday}}
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
 
-		<div class="symbin-grass">
-			<img :src="imgs.grass" />
-		</div>
+				<div class="symbin-grass">
+					<img :src="imgs.grass" />
+				</div>
 
-		<div class="symbin-baseinfo-main" :style="{background: ' url('+imgs.infoBg+') no-repeat center top',backgroundSize:'contain'}">
-			<div class="symbin-baseinfo-tag">
-				<img :src="imgs.cTag">
-				<span>
-					{{baseInfo.type === 1 ? '母鸡':'公鸡'}}
-				</span>
-			</div>
-			
-			<div class="symbin-allstep">
-				<h1>总步数</h1>
-				<div>
-					{{format(baseInfo.allCount)}}
-				</div>
-			</div>
+				<div class="symbin-baseinfo-main" :style="{background: ' url('+imgs.infoBg+') no-repeat center top',backgroundSize:'contain'}">
+					<div class="symbin-baseinfo-tag">
+						<img :src="imgs.cTag">
+						<span>
+							{{baseInfo.type === 1 ? '母鸡':'公鸡'}}
+						</span>
+					</div>
+					
+					<div class="symbin-allstep">
+						<h1>总步数</h1>
+						<div>
+							{{format(baseInfo.allCount)}}
+						</div>
+					</div>
 
-			<div class="symbin-c-info">
-				<div>
-					<div><span>12</span>km</div>
-					<div>总里程数</div>
-				</div>
-				<div>
-					<div><span>{{baseInfo.eggCount}}</span>枚/季度</div>
-					<div>平均产蛋数</div>
-				</div>
-				<div>
-					<div>不详</div>
-					<div>重量</div>
-				</div>
-			</div>
+					<div class="symbin-c-info">
+						<div>
+							<div><span>12</span>km</div>
+							<div>总里程数</div>
+						</div>
+						<div>
+							<div><span>{{baseInfo.eggCount}}</span>枚/季度</div>
+							<div>平均产蛋数</div>
+						</div>
+						<div>
+							<div>不详</div>
+							<div>重量</div>
+						</div>
+					</div>
+					<div class="symbin-baseinfo-chart">
+						<div class="symbin-chart-tags">
+							<div>湿度</div>
+							<div>温度</div>
+							<div>步数</div>
+						</div>
 
-		</div>
+						<div class="symbin-baseinfo-date">
+							{{baseInfo.steps && baseInfo.steps[stepIndex].date}}
+						</div>
+
+						<div class="symbin-baseinfo-step" v-if='baseInfo.steps'>
+							{{format(baseInfo.steps[stepIndex].step)}} <span>步</span>
+							<label> <em>{{(baseInfo.steps[stepIndex].step*.001*(baseInfo.type ===0?.11:baseInfo.steps[stepIndex].status === 0 ? .05:.1)).toFixed(2)}}</em> km</label>
+						</div>
+
+						<div class="symbin-temp-humidity"  v-if='baseInfo.steps'>
+							<div>
+								<span>温度</span>
+								<img :src="imgs.temperature">
+								<span>{{baseInfo.steps[stepIndex].temperature}}℃</span>
+							</div>
+							<div>
+								<span>湿度</span>
+								<img :src="imgs.humidity">
+								<span>{{baseInfo.steps[stepIndex].humidity}}RH%</span>
+							</div>
+						</div>
+						
+						<div class="symbin-chart">
+							
+							<canvas @touchend='touchend($event)' @touchmove='touchmove($event)' @touchstart='touchstart($event)' ref='canvas' width="620" height="400"></canvas>
+
+							<div class="symbin-line">
+								<img :src="imgs.line">
+							</div>
+
+						</div>
+
+						
+					</div>
+				</div>
+
+			</div>
+		</section>
+
 	</div>
 </template>
 
@@ -64,7 +109,9 @@
 	import './index.css';
 	import Header from '../header/index';
 
-	import symbinUtil from '../lib/util'
+	import symbinUtil from '../lib/util';
+	import IScroll from 'iscroll';
+	import {Step,Line} from './step';
 
 	import Vue from "vue";
 
@@ -75,6 +122,9 @@
 			return{
 				imgs:window.imgs,
 				viewH:document.documentElement.clientHeight,
+				stepIndex:12,
+				transX:0,
+				translateX:0,
 				baseInfo:{
 
 				}
@@ -98,18 +148,287 @@
 						})
 						s.baseInfo = data;
 						s.baseInfo.allCount = allCount;
+
+						s.initCanvas();
 					}
 				});
 			},
 			format(num){
 				return parseFloat(num).toLocaleString()
 			},
+			initCanvas(){
+				var canvas = this.$refs['canvas'];
+				/*
+					var myChart = echarts.init(canvas);
+					var app = {};
+					option = null;
+					var dataAxis = this.baseInfo.steps;
+					var data = dataAxis.map((item,i)=>{
+						return item.step;
+					});
+					var yMax =300;
+					var dataShadow = [];
+
+					for (var i = 0; i < data.length; i++) {
+					    dataShadow.push(yMax);
+					}
+
+					var option = {
+					    title: {
+					        text: '',
+					        subtext: ''
+					    },
+					    xAxis: {
+					        data: dataAxis.map((item,i)=>{return 'a'}),
+					        axisLabel: {
+					            inside: false,
+					            textStyle: {
+					                color: '#fff'
+					            }
+					        },
+					        axisTick: {
+					            show: false
+					        },
+					        axisLine: {
+					            show: false
+					        },
+					        z: 20
+					    },
+					    yAxis: {
+					        axisLine: {
+					            show: false
+					        },
+					        axisTick: {
+					            show: false
+					        },
+					        axisLabel: {
+					            show:false,
+					            textStyle: {
+					                color: '#f00'
+					            }
+					        }
+					    },
+					    dataZoom: [
+					        {
+					            type: 'inside'
+					        }
+					    ],
+					    series: [
+					        { // For shadow
+					            type: 'bar',
+					            itemStyle: {
+					                normal: {color: 'rgba(0,0,0,0.05)'}
+					            },
+					            barGap:'-100%',
+					            barCategoryGap:'40%',
+					            data: dataShadow,
+					            animation: false
+					        },
+					        {
+					            type: 'bar',
+					            itemStyle: {
+					                normal: {
+					                    color: new echarts.graphic.LinearGradient(
+					                        0, 0, 0, 1,
+					                        [
+					                            {offset: 0, color: '#83bff6'},
+					                            {offset: 0.5, color: '#188df0'},
+					                            {offset: 1, color: '#188df0'}
+					                        ]
+					                    )
+					                },
+					                emphasis: {
+					                    color: new echarts.graphic.LinearGradient(
+					                        0, 0, 0, 1,
+					                        [
+					                            {offset: 0, color: '#2378f7'},
+					                            {offset: 0.7, color: '#2378f7'},
+					                            {offset: 1, color: '#83bff6'}
+					                        ]
+					                    )
+					                }
+					            },
+					            data: data
+					        }
+					    ]
+					};
+
+					// Enable data zoom when user click bar.
+					var zoomSize = 2;
+					myChart.on('click', function (params) {
+					    console.log(dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)]);
+					    myChart.dispatchAction({
+					        type: 'dataZoom',
+					        startValue: dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)],
+					        endValue: dataAxis[Math.min(params.dataIndex + zoomSize / 2, data.length - 1)]
+					    });
+					});;
+					if (option && typeof option === "object") {
+					    myChart.setOption(option, true);
+					}
+				*/
+				var context = canvas.getContext('2d');
+				
+				var maxStep = 15000,
+					height = canvas.height;
+
+				this.context = context;
+				this.steps = [];
+				var minX = 0,
+					maxX = 0,
+					maxWidth = this.baseInfo.steps.length * 24 -4;
+				this.maxWidth = maxWidth;
+				this.drawTemperature();
+				this.baseInfo.steps.forEach((step,i)=>{
+					if(i === this.baseInfo.steps.length -1){
+						minX =(i*20+4*i  -( 620 - 20 )) + 600 / 2 - 10;
+					}
+					if(i === 0){
+						maxX = 620 / 2;
+					}
+					
+					
+				});
+
+
+
+				context.fillStyle = '#45c75d';
+				this.baseInfo.steps.forEach((step,i)=>{
+					var s = new Step({
+						context,
+						x:i*20+4*i,
+						y:height*(1-step.step/maxStep),
+						height:step.step/maxStep*height,
+					});
+					s.minX =i*20+4*i - minX;
+					s.maxX = i*20+4*i + maxX;
+					s.render();
+					this.steps.push(s);
+				});
+
+				
+
+			},
+			drawHumidity(){
+
+				var {context,maxWidth } = this;
+				context.strokeStyle = '#3eb1f3';
+
+				context.save();
+				context.translate(this.translateX,0);
+
+				this.context.beginPath();
+				var maxHumidity = 70;
+				var height = context.canvas.height;
+
+
+				this.baseInfo.steps.forEach((step,i)=>{
+					context[ i=== 0 ? 'moveTo' : 'lineTo'](i*24+10,(1-step.humidity/maxHumidity) * height);
+				});
+
+				
+				context.lineTo(maxWidth,(1-this.baseInfo.steps[this.baseInfo.steps.length-1].humidity/maxHumidity)*height);
+				context.lineTo(maxWidth,height);
+				context.lineTo(0,height);
+				context.closePath();
+
+
+				context.stroke();
+				context.restore();
+				
+			},
+			drawTemperature(){
+				
+				var {context,maxWidth } = this;
+				context.strokeStyle = '#f6c109';
+
+				context.save();
+				context.translate(this.translateX,0);
+
+
+
+				this.context.beginPath();
+				var maxTemperature = 42;
+				var height = context.canvas.height;
+
+
+				this.baseInfo.steps.forEach((step,i)=>{
+					context[i===0?'moveTo':'lineTo'](i*24+10,(1-step.temperature/maxTemperature) * height);
+				});
+
+				var grd = context.createLinearGradient(0,0,0,height);
+				grd.addColorStop(0,"#f6c109");
+				grd.addColorStop(1,"white");
+				context.lineTo(maxWidth,(1-this.baseInfo.steps[this.baseInfo.steps.length-1].temperature/maxTemperature)*height);
+				context.lineTo(maxWidth,height);
+				context.lineTo(0,height);
+				context.closePath();
+
+				context.fillStyle =grd;
+				context.fill();
+
+				context.stroke();
+				context.restore();
+
+				this.drawHumidity()
+				
+			},
+			touchstart(e){
+				var e =  e.changedTouches[0];
+				this.startX = e.pageX;
+			},
+			touchmove(e){
+				e.preventDefault();
+				var e =  e.changedTouches[0];
+				this.transX =  e.pageX - this.startX;
+				var height = this.context.canvas.height;
+				
+				this.context.clearRect(0,0,365*24,height);
+
+
+				this.lastX = this.lastX || 0;
+				this.drawTemperature();
+
+				this.steps.forEach((step,i)=>{
+					//
+
+					step.x += this.transX/10;
+					if(step.x >= step.maxX){
+						step.x = step.maxX;
+					}
+					step.x <= step.minX && (step.x = step.minX);
+					if(i === 0){
+						this.stepIndex = Math.abs(((step.x - step.maxX) / 24) | 0);
+					}
+					step.render();
+				});
+
+				this.translateX = this.steps[0].x;
+				
+
+				this.lastX = e.pageX;
+
+				return false
+ 
+			},
+			touchend(e){
+				console.log(this.translateX,this.steps[0].x )
+			}
 			
 
 		},
 		mounted(){
 
 			this.loadInfoById();
+
+
+
+			this.scroll = new IScroll(this.$refs['page'],{scrollbars:true});
+
+			setTimeout(()=>{
+				this.scroll.refresh();
+				this.scroll.scrollTo(0,-600,1)
+			},1000)
  			
 		}
 	}
